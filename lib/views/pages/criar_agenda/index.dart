3fb10932b1d3c6426bd9c953c57e959ef4ee7c51
TccTunico <79997046+TccTunico@users.noreply.github.com>
2021-03-04 12:05:00 -0300

    Update index.dart

--- lib/views/pages/criar_agenda/index.dart
+++ lib/views/pages/criar_agenda/index.dart
@@ -1,6 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:tunicoapp/views/components/flat_button_ext/index.dart';
-import 'package:tunicoapp/views/components/text_form_field_ext/index.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:tunicoapp/views/components/flat_button_ext/index.dart';
 import 'package:tunicoapp/views/components/text_form_field_ext/index.dart';
 
@@ -11,7 +11,8 @@ class CriarAgendaPage extends StatefulWidget {
 
 class _CriarAgendaPageState extends State<CriarAgendaPage> {
   final _formKey = GlobalKey<FormState>();
-
+   final imagePicker = ImagePicker();
+  String foto= '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,17 +32,62 @@ class _CriarAgendaPageState extends State<CriarAgendaPage> {
             child: ListView(
               shrinkWrap: true,
               children: [
-                CircleAvatar(
-                  backgroundColor: Colors.transparent,
-                  radius: 100,
-                  child: Image.asset('assets/images/grupo_pessoas.png'),
+                 Padding(
+                padding: const EdgeInsets.all(10),
+                child: Stack(
+                  children: [
+                    Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                          width: 200,
+                          height: 200,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(image: _retornaFoto(foto), 
+                                ),
+                          )),
+                    ),
+                    Center(
+                      child: Container(
+                        color: Colors.transparent,
+                        width: 250,
+                        height: 190,
+                        child: Align(
+                          alignment: Alignment.topRight,
+                          child: PopupMenuButton<int>(
+                            itemBuilder: (context) => <PopupMenuEntry<int>>[
+                              const PopupMenuItem<int>(
+                                child: Text('Câmera'),
+                                value: 0,
+                              ),
+                              const PopupMenuItem<int>(
+                                child: Text('Galeria'),
+                                value: 1,
+                              )
+                            ],
+                            onSelected: (valor) async {
+                              final fotoSelecionada =
+                                  await imagePicker.getImage(
+                                      source: (valor == 0
+                                          ? ImageSource.camera
+                                          : ImageSource.gallery));
+                              setState(() {
+                               foto = fotoSelecionada.path;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
+              ),
                 Padding(
                   padding: EdgeInsets.fromLTRB(40, 30, 40, 0),
                   child: TextFormFieldExt(
                       labelText: 'Nome da agenda',
                       keyboardType: TextInputType.text,
-                      prefixIcon: Icon(Icons.edit),
+                      prefixIcon: Icon(Icons.edit, color: Color.fromRGBO(255, 148, 88, 1)),
                       //controller: ,
                       validator: (text) {
                         if (text.trim().length == 0)
@@ -53,7 +99,7 @@ class _CriarAgendaPageState extends State<CriarAgendaPage> {
                   child: TextFormFieldExt(
                       labelText: 'Descrição',
                       keyboardType: TextInputType.text,
-                      prefixIcon: Icon(Icons.description_outlined),
+                      prefixIcon: Icon(Icons.description_outlined, color: Color.fromRGBO(255, 148, 88, 1)),
                       //controller: ,
                       validator: (text) {
                         if (text.trim().length == 0)
@@ -65,7 +111,7 @@ class _CriarAgendaPageState extends State<CriarAgendaPage> {
                   child: TextFormFieldExt(
                     labelText: 'Senha (opcional)',
                     keyboardType: TextInputType.text,
-                    prefixIcon: Icon(Icons.vpn_key),
+                    prefixIcon: Icon(Icons.vpn_key, color: Color.fromRGBO(255, 148, 88, 1)),
                     obscureText: true,
                     //controller: ,
                   ),
@@ -75,7 +121,7 @@ class _CriarAgendaPageState extends State<CriarAgendaPage> {
                   child: TextFormFieldExt(
                     labelText: 'N° de participantes (máx XX)',
                     keyboardType: TextInputType.number,
-                    prefixIcon: Icon(Icons.people_alt_outlined),
+                    prefixIcon: Icon(Icons.people_alt_outlined, color: Color.fromRGBO(255, 148, 88, 1)),
                     obscureText: true,
                     //controller: ,
                   ),
@@ -94,5 +140,24 @@ class _CriarAgendaPageState extends State<CriarAgendaPage> {
             )),
       ),
     );
+
+  }
+  dynamic _retornaFoto(String caminho) {
+    if (caminho.length == 0) {
+      return AssetImage('');
+    } else {
+      if (caminho.contains('http'))
+        try {
+          return NetworkImage(caminho);
+        } catch (ex) {
+          return AssetImage('');
+        }
+      else
+        try {
+          return FileImage(File(caminho));
+        } catch (ex) {
+          return AssetImage('');
+        }
+    }
   }
 }
